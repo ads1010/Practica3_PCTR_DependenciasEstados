0fd0179cc68d399e2a9badd6e79b49b433f52452
alvar <alvar@DESKTOP-I236G7O>
2022-03-28 09:35:11 +0200

    Cambio nombre atributo NUMSALIDAS, implementación método run() ActividadSalidaPuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,10 +1,15 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ActividadSalidaPuerta implements Runnable{
 	//
 	// TODO
 	//
-	 private static final int NUMENTRADAS = 20;
+	 private static final int NUMSALIDAS = 20;
 	 private String puerta;
 	 private IParque parque;
 	
@@ -17,6 +22,16 @@ public class ActividadSalidaPuerta implements Runnable{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < NUMSALIDAS; i ++) {
+			try {
+				parque.salirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
 		
 	}
 }
